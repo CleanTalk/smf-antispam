2185ea3ac9a6d226961e971469e14b36bc0c7892
rustik <psrustik@gmail.com>
2014-08-02 10:21:36 +0600

    PSR1/2 codestyle

--- CleantalkMod.php
+++ CleantalkMod.php
@@ -9,8 +9,9 @@
  * @license GNU/GPL: http://www.gnu.org/copyleft/gpl.html
  */
 
-if (!defined('SMF'))
-	die('Hacking attempt...');
+if (!defined('SMF')) {
+    die('Hacking attempt...');
+}
 
 require_once(dirname(__FILE__) . '/cleantalk.class.php');
 
@@ -27,75 +28,77 @@ define('CT_SERVER_URL', 'http://moderate.cleantalk.ru');
  */
 function cleantalk_check_register(&$regOptions, $theme_vars)
 {
-	global $language, $user_info, $modSettings;
+    global $language, $user_info, $modSettings;
 
-	if ($regOptions['interface'] == 'admin') {
-		return;
-	}
+    if ($regOptions['interface'] == 'admin') {
+        return;
+    }
 
-	$ct = new Cleantalk();
-	$ct->server_url = CT_SERVER_URL;
+    $ct = new Cleantalk();
+    $ct->server_url = CT_SERVER_URL;
 
-	$ct_request = new CleantalkRequest();
-	$ct_request->auth_key = cleantalk_get_api_key();
+    $ct_request = new CleantalkRequest();
+    $ct_request->auth_key = cleantalk_get_api_key();
 
-	$ct_request->response_lang = 'en'; // SMF use any charset and language
+    $ct_request->response_lang = 'en'; // SMF use any charset and language
 
-	$ct_request->agent = CT_AGENT_VERSION;
-	$ct_request->sender_email = isset($regOptions['email']) ? $regOptions['email'] : '';
+    $ct_request->agent = CT_AGENT_VERSION;
+    $ct_request->sender_email = isset($regOptions['email']) ? $regOptions['email'] : '';
 
-	$ip = isset($regOptions['register_vars']['member_ip']) ? $regOptions['register_vars']['member_ip'] : $_SERVER['REMOTE_ADDR'];
-	$ct_request->sender_ip =  $ct->ct_session_ip($ip);
+    $ip = isset($regOptions['register_vars']['member_ip']) ? $regOptions['register_vars']['member_ip'] : $_SERVER['REMOTE_ADDR'];
+    $ct_request->sender_ip = $ct->ct_session_ip($ip);
 
-	$ct_request->sender_nickname = isset($regOptions['username']) ? $regOptions['username'] : '';
+    $ct_request->sender_nickname = isset($regOptions['username']) ? $regOptions['username'] : '';
 
-	if (isset($_SESSION['cleantalk_registration_form_start_time'])) {
-		$ct_request->submit_time = time() - $_SESSION['cleantalk_registration_form_start_time'];
-	}
+    if (isset($_SESSION['cleantalk_registration_form_start_time'])) {
+        $ct_request->submit_time = time() - $_SESSION['cleantalk_registration_form_start_time'];
+    }
 
-	if (isset($_POST['ct_checkjs'])) {
-		$ct_request->js_on = $_POST['ct_checkjs'] == cleantalk_get_checkjs_code() ? 1 : 0;
-	}
+    if (isset($_POST['ct_checkjs'])) {
+        $ct_request->js_on = $_POST['ct_checkjs'] == cleantalk_get_checkjs_code() ? 1 : 0;
+    }
 
-	$ct_request->sender_info = json_encode(array(
-		'REFFERRER' => isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null,
-		'cms_lang' => substr($language, 0, 2),
-		'USER_AGENT' => isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : null,
-	));
+    $ct_request->sender_info = json_encode(
+        array(
+            'REFFERRER' => isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null,
+            'cms_lang' => substr($language, 0, 2),
+            'USER_AGENT' => isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : null,
+        )
+    );
 
-	/**
-	 * @var CleantalkResponse $ct_result CleanTalk API call result
-	 */
-	$ct_result = $ct->isAllowUser($ct_request);
+    /**
+     * @var CleantalkResponse $ct_result CleanTalk API call result
+     */
+    $ct_result = $ct->isAllowUser($ct_request);
 
-	if ($ct_result->inactive == 1) {
-		// need admin approval
+    if ($ct_result->inactive == 1) {
+        // need admin approval
 
-		log_error('CleanTalk: need approval for "' . $regOptions['username'] . '"', 'user');
+        log_error('CleanTalk: need approval for "' . $regOptions['username'] . '"', 'user');
 
-		$regOptions['register_vars']['is_activated'] = 3; // waiting for admin approval
-		$regOptions['require'] = 'approval';
+        $regOptions['register_vars']['is_activated'] = 3; // waiting for admin approval
+        $regOptions['require'] = 'approval';
 
-		if (!isset($modSettings['notify_new_registration']) || empty($modSettings['notify_new_registration'])) {
-			// temporarly turn on notify for new registration
-			$modSettings['notify_new_registration'] = 1;
-		}
+        if (!isset($modSettings['notify_new_registration']) || empty($modSettings['notify_new_registration'])) {
+            // temporarly turn on notify for new registration
+            $modSettings['notify_new_registration'] = 1;
+        }
 
-		// add Cleantalk message to email template
-		$user_info['cleantalkmessage'] = $ct_result->comment;
+        // add Cleantalk message to email template
+        $user_info['cleantalkmessage'] = $ct_result->comment;
 
-		// temporarly turn on registration_method to approval_after
-		$modSettings['registration_method'] = 2;
-		return;
-	}
+        // temporarly turn on registration_method to approval_after
+        $modSettings['registration_method'] = 2;
+        return;
+    }
 
-	if ($ct_result->allow == 0) {
-		// this is bot, stop registration
-		fatal_error('CleanTalk: ' . strip_tags($ct_result->comment), 'user');
-	} else {
-		// all ok, only logging
-		log_error('CleanTalk: allow regisration for "' . $regOptions['username'] . '"', 'user');
-	}
+    if ($ct_result->allow == 0) {
+        // this is bot, stop registration
+        fatal_error('CleanTalk: ' . strip_tags($ct_result->comment), 'user');
+    } else {
+        // all ok, only logging
+        log_error('CleanTalk: allow regisration for "' . $regOptions['username'] . '"', 'user');
+    }
 }
 
 /**
@@ -104,9 +107,9 @@ function cleantalk_check_register(&$regOptions, $theme_vars)
  */
 function cleantalk_get_checkjs_code()
 {
-	global $webmaster_email;
+    global $webmaster_email;
 
-	return md5(cleantalk_get_api_key() . $webmaster_email);
+    return md5(cleantalk_get_api_key() . $webmaster_email);
 }
 
 /**
@@ -115,9 +118,9 @@ function cleantalk_get_checkjs_code()
  */
 function cleantalk_get_api_key()
 {
-	global $modSettings;
+    global $modSettings;
 
-	return isset($modSettings['cleantalk_api_key']) ? $modSettings['cleantalk_api_key'] : null;
+    return isset($modSettings['cleantalk_api_key']) ? $modSettings['cleantalk_api_key'] : null;
 }
 
 /**
@@ -126,7 +129,7 @@ function cleantalk_get_api_key()
  */
 function cleantalk_general_mod_settings(&$config_vars)
 {
-	$config_vars[] = array('title', 'cleantalk_settings');
-	$config_vars[] = array('text', 'cleantalk_api_key');
-	$config_vars[] = array('desc', 'cleantalk_api_key_description');
+    $config_vars[] = array('title', 'cleantalk_settings');
+    $config_vars[] = array('text', 'cleantalk_api_key');
+    $config_vars[] = array('desc', 'cleantalk_api_key_description');
 }
--- install.php
+++ install.php
@@ -10,30 +10,30 @@
  */
 
 $hooks = array(
-	'integrate_pre_include' => '$sourcedir/cleantalk/CleantalkMod.php',
-	'integrate_register' => 'cleantalk_check_register',
-	'integrate_general_mod_settings' => 'cleantalk_general_mod_settings',
+    'integrate_pre_include' => '$sourcedir/cleantalk/CleantalkMod.php',
+    'integrate_register' => 'cleantalk_check_register',
+    'integrate_general_mod_settings' => 'cleantalk_general_mod_settings',
 );
 
 $isInstalling = empty($context['uninstalling']);
 
 // set integration hooks
 foreach ($hooks as $hook => $function) {
-	if ($isInstalling) {
-		add_integration_function($hook, $function);
-	} else {
-		remove_integration_function($hook, $function);
-	}
+    if ($isInstalling) {
+        add_integration_function($hook, $function);
+    } else {
+        remove_integration_function($hook, $function);
+    }
 }
 
 if ($isInstalling) {
-	// Anti-Spam Verification captcha disable
-	updateSettings(array('reg_verification' => '0'), true);
+    // Anti-Spam Verification captcha disable
+    updateSettings(array('reg_verification' => '0'), true);
 
-	$oldKey = isset($modSettings['cleantalk_api_key']) ?  $modSettings['cleantalk_api_key'] : '';
+    $oldKey = isset($modSettings['cleantalk_api_key']) ? $modSettings['cleantalk_api_key'] : '';
 
-	updateSettings(array('cleantalk_api_key' => $oldKey), false);
+    updateSettings(array('cleantalk_api_key' => $oldKey), false);
 } else {
-	// Anti-Spam Verification captcha
-	updateSettings(array('reg_verification' => '1'), true);
+    // Anti-Spam Verification captcha
+    updateSettings(array('reg_verification' => '1'), true);
 }
