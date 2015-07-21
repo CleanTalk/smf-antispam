bcc24d6847702c3f2ab9d1a29f813370df85d910
rustik <psrustik@gmail.com>
2015-07-21 23:45:06 +0500

    1.40 version

--- RoboFile.php
+++ RoboFile.php
@@ -8,7 +8,7 @@
 class RoboFile extends \Robo\Tasks
 {
     const PACKAGE = 'antispam_cleantalk_smf';
-    const VERSION = '1.40-beta';
+    const VERSION = '1.40';
 
     /**
      * Build SMF zip-package
