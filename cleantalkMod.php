56ff2ba666b0b2bac7c0bf7ab18640ae99c908d5
davydovct <davydovct@cleantalk.org>
2019-09-30 16:50:29 +0500

    fix convert to int

--- cleantalkMod.php
+++ cleantalkMod.php
@@ -752,7 +752,7 @@ function cleantalk_get_checkjs_code(){
         foreach ($keys as $k => $t) {
 
             // Removing key if it's to old
-            if (time() - $t > $js_keys['js_keys_amount'] * 86400) {
+            if (time() - (int)$t > $js_keys['js_keys_amount'] * 86400) {
                 unset($keys[$k]);
                 continue;
             }
@@ -763,7 +763,7 @@ function cleantalk_get_checkjs_code(){
             }
         }
         // Get new key if the latest key is too old
-        if (time() - $latest_key_time > $js_keys['js_key_lifetime']) {
+        if (time() - (int)$latest_key_time > $js_keys['js_key_lifetime']) {
             $keys[$key] = time();
         }           
     }
